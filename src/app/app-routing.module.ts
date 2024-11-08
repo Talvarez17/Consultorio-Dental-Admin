472e9e5a25bc39f132d3ea6b1944122781e16141
Tomas Alvarez <soytalvarez17@gmail.com>
2024-11-08 13:57:58 -0600

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { PacientesComponent } from './pages/auth/pacientes/pacientes.component';
+import { CitasComponent } from './pages/auth/citas/citas.component';
+import { ConsultasComponent } from './pages/auth/consultas/consultas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", children: [
+      { path: "login", component: LoginComponent },
+      { path: "pacientes", component: PacientesComponent },
+      { path: "citas", component: CitasComponent },
+      { path: "consultas", component: ConsultasComponent },
+
+      { path: "**", redirectTo: "login" },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PacientesComponent } from './pages/auth/pacientes/pacientes.component';
+import { CitasComponent } from './pages/auth/citas/citas.component';
+import { ConsultasComponent } from './pages/auth/consultas/consultas.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PacientesComponent,
+    CitasComponent,
+    ConsultasComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
