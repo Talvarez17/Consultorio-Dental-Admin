ad6001bf67a5f1ad7751ffec79a6cfd0438f6db5
Tomas Alvarez <soytalvarez17@gmail.com>
2024-11-15 14:31:42 -0600

    funcionabilidad pacientes

--- src/app/pages/auth/pacientes/pacientes.component.ts
+++ src/app/pages/auth/pacientes/pacientes.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ConexionService } from 'src/app/services/conexion.service';
+import { v4 as uuidv4 } from 'uuid';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-pacientes',
@@ -6,5 +11,207 @@ import { Component } from '@angular/core';
   styleUrls: ['./pacientes.component.css']
 })
 export class PacientesComponent {
+
+  uudi = uuidv4();
+  pacientes: any = [];
+  pacienteSolo: any = [];
+
+  FormularioA: FormGroup = this.fb.group({
+    _id: this.uudi,
+    nombre: [, [Validators.required, Validators.maxLength(50), Validators.minLength(2)]],
+    apellidoPaterno: [, [Validators.required, Validators.maxLength(50), Validators.minLength(2)]],
+    apellidoMaterno: [, [Validators.required, Validators.maxLength(50), Validators.minLength(2)]],
+    fechaNacimiento: [, Validators.required],
+    telefono: [, [Validators.required, Validators.maxLength(10), Validators.minLength(10), Validators.pattern(/^\d{10}$/)]],
+    correo: [, [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
+    contrasenia: [, Validators.required]
+  });
+
+  FormularioE: FormGroup = this.fb.group({
+    id:[],
+    nombre: [, [Validators.required, Validators.maxLength(50), Validators.minLength(2)]],
+    apellidoPaterno: [, [Validators.required, Validators.maxLength(50), Validators.minLength(2)]],
+    apellidoMaterno: [, [Validators.required, Validators.maxLength(50), Validators.minLength(2)]],
+    fechaNacimiento: [, Validators.required],
+    telefono: [, [Validators.required, Validators.maxLength(10), Validators.minLength(10), Validators.pattern(/^\d{10}$/)]],
+    correo: [, [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
+    contrasenia: [, Validators.required]
+  });
+
+
+  constructor(public router: Router, private fb: FormBuilder, public service: ConexionService) {
+  }
+
+  ngOnInit() { this.obtenerPacientes() }
+
+  calcularEdad(fechaNacimiento: string): number {
+    const hoy = new Date();
+    const fechaNac = new Date(fechaNacimiento);
+    let edad = hoy.getFullYear() - fechaNac.getFullYear();
+    const mes = hoy.getMonth() - fechaNac.getMonth();
+
+    if (mes < 0 || (mes === 0 && hoy.getDate() < fechaNac.getDate())) {
+      edad--;
+    }
+
+    return edad;
+  }
+
+  @ViewChild('editarPaciente') editarPaciente!: ElementRef;
+
+  abrirModalYObtenerPaciente(id: any) {
+    this.editarPaciente.nativeElement.showModal();
+    this.obtenerUnPaciente(id);
+  }
+
+  obtenerPacientes() {
+    this.service.get('paciente/getAll').subscribe((info: any) => {
+
+      console.log(info.data)
+      if (info) {
+        this.pacientes = info.data;
+      }
+    })
+  }
+
+  obtenerUnPaciente(id: any) {
+    this.service.get(`paciente/getOne/${id}`).subscribe((info: any) => {
+
+      if (info.error == false) {
+        this.FormularioE.patchValue({
+          id: info.data.id,
+          nombre: info.data.nombre,
+          apellidoPaterno: info.data.apellidoPaterno,
+          apellidoMaterno: info.data.apellidoMaterno,
+          fechaNacimiento: info.data.fechaNacimiento,
+          telefono: info.data.telefono,
+          correo: info.data.correo,
+          contrasenia: null
+        });
+
+        this.pacienteSolo = info.data.id;
+      }
+    })
+  }
+
+  actualizarPaciente(id:any) {
+
+    this.service.put(`paciente/update/${id}`, this.FormularioE.value).subscribe((info: any) => {
+
+      console.log(this.FormularioA.value);
+      
+
+      if (info.error == false) {
+
+        Swal.fire({
+          icon: "success",
+          title: "Exito",
+          text: "Registro actualizado con exito",
+          showConfirmButton: false,
+          timer: 1500
+        });
+
+        setTimeout(() => {
+          location.reload();
+        }, 1500);
+      }
+      else {
+        Swal.fire({
+          icon: "error",
+          title: "Upsss",
+          text: "Hubo un error al intentar actualiza el registro, intentelo de nuevo",
+          showConfirmButton: false,
+          timer: 1500
+        });
+      };
+    });
+
+  }
+
+  agregarPaciente() {
+
+    this.service.post('paciente/insert', this.FormularioA.value).subscribe((info: any) => {
+
+      if (info.error == false) {
+        console.log(info.data);
+
+        Swal.fire({
+          icon: "success",
+          title: "Exito",
+          text: "Registro creado con exito",
+          showConfirmButton: false,
+          timer: 1500
+        });
+
+        setTimeout(() => {
+          location.reload();
+        }, 1500);
+      }
+      else {
+        Swal.fire({
+          icon: "error",
+          title: "Upsss",
+          text: "Hubo un error al intentar crear el registro, intentelo de nuevo",
+          showConfirmButton: false,
+          timer: 1500
+        });
+      };
+    });
+
+  }
+
+  eliminarPaciente(id: any) {
+
+    Swal.fire({
+      title: "¿Quieres eliminar este registro?",
+      text: "No podras recuperarlo",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#14B8A6",
+      cancelButtonColor: "#EF4444",
+      confirmButtonText: "Aceptar",
+      cancelButtonText: "Cancelar"
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        this.service.delete(`paciente/delete/${id}`).subscribe((info: any) => {
+
+          if (info.error == false) {
+            Swal.fire({
+              icon: "success",
+              title: "Exito",
+              text: "Registro eliminado con exito",
+              showConfirmButton: false,
+              timer: 1500
+            });
+
+            setTimeout(() => {
+              location.reload();
+            }, 1500);
+
+          } else {
+            Swal.fire({
+              icon: "error",
+              title: "Upsss",
+              text: "Hubo un error al intentar eliminar el registro, intentelo de nuevo",
+              showConfirmButton: false,
+              timer: 1500
+            });
+
+          }
+        })
+
+      }
+    });
+
+  }
+
+  campoValidoAgregar(campo: string) {
+    return this.FormularioA.controls[campo].errors && this.FormularioA.controls[campo].touched;
+  };
+
+  campoValidoEditar(campo: string) {
+    return this.FormularioE.controls[campo].errors && this.FormularioE.controls[campo].touched;
+  };
 
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ConexionService } from 'src/app/services/conexion.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-login',
@@ -8,13 +11,41 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
-  ngOnInit() {
+  Login: FormGroup = this.fb.group({
+    correo: [, Validators.required],
+    contrasenia: [, Validators.required],
+  });
+
+  constructor(public router: Router, private fb: FormBuilder, public service: ConexionService) {
+    localStorage.clear();
   }
 
-  constructor(public router: Router) {}
+  ngOnInit() { }
 
   login() {
-    this.router.navigate(['/pacientes']);
+
+    this.service.post('doctor/login', this.Login.value).subscribe((info: any) => {
+
+      if (info.error == false) {
+        localStorage.setItem("id", info.data);
+        this.router.navigate(['/pacientes']);
+      }
+      else {
+        Swal.fire({
+          icon: "error",
+          title: "Correo o contraseña incorrectos",
+          text: "Verifique que esten escritos correctamente",
+          showConfirmButton: false,
+          timer: 1500
+        });
+      };
+    });
+
   }
+
+  campoValido(campo: string) {
+    return this.Login.controls[campo].errors && this.Login.controls[campo].touched;
+  };
+
 
 }
